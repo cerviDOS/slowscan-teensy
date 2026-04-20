a59efbbfb72fb406e1cca191a6d0e750aba9b632
enosco <scottrweno@gmail.com>
2026-04-20 11:04:36 -0400

    Updated FrequencyDemodulator.h to match changes in source file

--- src/FrequencyDemodulator.h
+++ src/FrequencyDemodulator.h
@@ -1,9 +1,6 @@
 #include <stdint.h>
 #include <arduinoFFT.h>
 
-#include "Array.h"
-
-
 class FrequencyDemodulator {
 public:
     static const uint16_t BUFFER_SIZE = 1024;
@@ -13,18 +10,22 @@ public:
     // Calculates the analytic signal of a waveform.
     // waveform_real[] will remain unchanged.
     // waveform_imag[] will contain waveform 90-degrees out of phase (in quadrature) with waveform_real[].
-    void analytic_signal(double waveform_real[BUFFER_SIZE], double waveform_imag[BUFFER_SIZE], bool window = true);
+    void analytic_signal(double waveform_real[BUFFER_SIZE],
+                         double waveform_imag[BUFFER_SIZE],
+                         bool window = true);
 
     // Calculates the instantaneous frequencies of a waveform
     // and stores the result in frequency_data[].
-    void inst_freq(const double waveform_data[BUFFER_SIZE], double frequency_data[BUFFER_SIZE-1]);
+    void frequencies(const double waveform_data[BUFFER_SIZE],
+                     double frequency_data[BUFFER_SIZE-1]);
 
 private:
+
+    double instantaneous_frequency(double real1, double imag1,
+                                   double real2, double imag2);
 
     double recover_phase(double I, double Q);
 
     uint32_t m_sample_rate;
-
-
     ArduinoFFT<double> fft;
 };
